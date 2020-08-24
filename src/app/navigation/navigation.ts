7b768a338d85c6f8e8f3e0df1d664796ff073c0c
Mauro Gómez <mauro.f.gmz@gmail.com>
2020-08-23 23:10:50 -0300

    test navigation

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -22,5 +22,27 @@ export const navigation: FuseNavigation[] = [
                 }
             }
         ]
+    },
+    {
+        id       : 'applications',
+        title    : 'Applications',
+        translate: 'NAV.APPLICATIONS',
+        type     : 'group',
+        children : [
+            {
+                id       : 'home',
+                title    : 'Home',
+                translate: 'NAV.SAMPLE.TITLE',
+                type     : 'item',
+                icon     : 'photo',
+                url      : '/sample',
+                badge    : {
+                    title    : '25',
+                    translate: 'NAV.SAMPLE.BADGE',
+                    bg       : '#F44336',
+                    fg       : '#FFFFFF'
+                }
+            }
+        ]
     }
 ];
