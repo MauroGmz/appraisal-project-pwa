e3588805bbc83afe12f8e493c8bb60ad5900c50a
Mauro Gómez <mauro.f.gmz@gmail.com>
2020-08-29 16:51:40 -0300

    finished uploading photos using the camera

--- src/app/main/appraisal/appraisal.component.ts
+++ src/app/main/appraisal/appraisal.component.ts
@@ -9,6 +9,7 @@ export class AppraisalComponent implements OnInit {
   
   panelOpenState = false;
   step = 0;
+  flag = false;
 
   constructor() { }
 
@@ -26,6 +27,29 @@ export class AppraisalComponent implements OnInit {
 
   prevStep() {
     this.step--;
+  }
+
+  
+  activeCamera(camera) {
+    camera.click();
+  }
+
+  onChange(evt, photo) {
+    this.flag = true;
+    // Create url object and show Photo from BLOB Object.
+    photo.src = URL.createObjectURL(evt.target.files[0]);
+    // Create Http Request Instance.
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) console.log(xhttp.responseText);
+    };
+    // Create Data Form Instance.
+    const formData = new FormData();
+    // Add blob object into instance.
+    formData.append("photo", evt.target.files[0]);
+    // Open and send data to endpoint /upload
+    /*xhttp.open('POST', window.location.href + 'upload', true);
+    xhttp.send(formData)*/
   }
 
 }
--- src/app/main/appraisal/appraisal.module.ts
+++ src/app/main/appraisal/appraisal.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AppraisalComponent } from './appraisal.component';
 
@@ -7,7 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';  
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';   
 
 const routes: Routes = [
     {
@@ -22,12 +24,14 @@ const routes: Routes = [
     ],
     imports        : [
         RouterModule.forChild(routes),
+        CommonModule,
         MatExpansionModule,
         MatButtonModule,
         MatInputModule,
         MatFormFieldModule,
         MatCardModule,
-        MatIconModule
+        MatIconModule,
+        MatGridListModule
     ],
     exports     : [
         AppraisalComponent
