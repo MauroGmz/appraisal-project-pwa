2702d460170a8b674a5b6b27fc7a2f9112c37e84
Mauro Gómez <mauro.f.gmz@gmail.com>
2020-09-07 16:11:51 -0300

    Done Example of user appraisal list view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatCardModule } from '@angular/material/card'; 
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import 'hammerjs';
 import '../polyfills'
@@ -58,6 +59,7 @@ const appRoutes: Routes = [
         // Material
         MatButtonModule,
         MatIconModule,
+        MatCardModule,
 
         // Fuse modules
         FuseModule.forRoot(fuseConfig),
